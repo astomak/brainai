31fc2be04cc5e4e1e64cf13b7931cbb86995f88b
astomak <harshakaastomak@gmail.com>
2020-07-25 12:50:31 +0530

    Updates

---
+++ precache-manifest.0960540f2b38b6d3e53051d2ecbdfebd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed532c5189a458749df1be72e49d353d",
+    "url": "/brainai/index.html"
+  },
+  {
+    "revision": "078910378c82702fdd05",
+    "url": "/brainai/static/css/main.6905a768.chunk.css"
+  },
+  {
+    "revision": "8c0dbe0a9551cef2071b",
+    "url": "/brainai/static/js/2.ddd6fa32.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/brainai/static/js/2.ddd6fa32.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "078910378c82702fdd05",
+    "url": "/brainai/static/js/main.27abcc6e.chunk.js"
+  },
+  {
+    "revision": "f00234b81572b7233240",
+    "url": "/brainai/static/js/runtime-main.710acd10.js"
+  }
+]);
