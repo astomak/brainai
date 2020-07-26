31db28e681ec0872ecedab476725ae0125612e13
astomak <65339510+astomak@users.noreply.github.com>
2020-07-26 13:08:21 +0530

    Awesome

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Navigation from './components/Navigation/Navigation'
+import './App.css';
+import './tailwind.output.css';
+
+function App() {
+  return (
+    <div>
+      < Navigation />
+    </div>
+  );
+}
+
+export default App;
